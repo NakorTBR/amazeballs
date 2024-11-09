2976481e13cc6987054568e86b2d5284313d813e
NakorTBR <nakor_@hotmail.com>
2024-11-09 13:36:26 -0500

    Added line and point classes.  Added the ability to draw lines.

---
+++ line.py
@@ -0,0 +1,11 @@
+from point import Point
+from tkinter import Canvas
+
+class Line():
+    def __init__(self, start: Point, end: Point):
+        self.start = start
+        self.end = end
+    
+
+    def draw(self, canvas: Canvas, fill_colour: str):
+        canvas.create_line(self.start.x, self.start.y, self.end.x, self.end.y, fill=fill_colour, width=2)
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from window import Window
+from point import Point
+from line import Line
 
 def main():
     win = Window(800, 600)
+
+    p1 = Point(0, 0)
+    p2 = Point(800, 600)
+    l1 = Line(p1, p2)
+    win.draw_line(l1, "pink")
+    p3 = Point(0, 600)
+    p4 = Point(800, 0)
+    l2 = Line(p3, p4)
+    win.draw_line(l2, "red")
+
     win.wait_for_close()
 
 
